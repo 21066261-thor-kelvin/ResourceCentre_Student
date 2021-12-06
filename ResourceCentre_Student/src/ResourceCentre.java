386d44e9cf7c983de86947c23134ccb8b5b73c10
Sheng Jun <21066659@myrp.edu.sg>
2021-12-06 16:39:26 +0800

    Missing code input

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -191,7 +191,7 @@ public class ResourceCentre {
 	public static void addChromebook(ArrayList<Chromebook> chromebookList, Chromebook cb) {
 		// write your code here
 		chromebookList.add(cb);
-		System.out.println("Chromebook added");
+		System.out.println("Chromebook addedd");
 	}
 	
 	//================================= Option 3 Loan an item (CRUD - Update) =================================
