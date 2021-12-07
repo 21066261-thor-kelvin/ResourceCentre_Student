f8f6081ff78280557359a8e4febb197e86728370
sjdude2 <21066659@myrp.edu.sg>
2021-12-07 10:30:37 +0800

    updated with Chromebook code

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -189,7 +189,7 @@ public class ResourceCentre {
 		
 	}	
 	public static void addChromebook(ArrayList<Chromebook> chromebookList, Chromebook cb) {
-		// write your code here
+		//// write your code here
 		chromebookList.add(cb);
 		System.out.println("Chromebook added");
 	}
